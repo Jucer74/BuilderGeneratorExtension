56baa735f8f2ba74b574f6e7ff34093b67292372
Julio Robles <Jucer74@Hotmail.com>
2022-07-18 02:05:41 -0500

    Update

--- Code/src/Extension/Commands/BuilderCommand.cs
+++ Code/src/Extension/Commands/BuilderCommand.cs
@@ -1,5 +1,4 @@
 ﻿using BuilderGeneratorFactory;
-using BuilderGeneratorFactory.Define;
 using BuilderGeneratorFactory.Exceptions;
 using BuilderGeneratorFactory.Options;
 using EnvDTE;
@@ -7,7 +6,6 @@ using EnvDTE80;
 using Microsoft.VisualStudio.Shell.Interop;
 using System.IO;
 using System.Threading.Tasks;
-using static BuilderGenerator.OptionsProvider;
 
 namespace BuilderGenerator
 {
@@ -16,8 +14,13 @@ namespace BuilderGenerator
    {
       protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
       {
+         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
          try
          {
+            //var dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
+            var dte2 = ServiceProvider.GlobalProvider.GetService(typeof(SDTE)) as DTE2;
+
             var options = BuilderGeneratorOptions.Instance;
 
             BuilderOptions builderOptions = new BuilderOptions()
@@ -26,7 +29,7 @@ namespace BuilderGenerator
                EnableAutoFixture = options.EnableAutoFixture
             };
 
-            string fileClassName = await GetFileClassNameAsync();
+            string fileClassName = await GetFileClassNameAsync(dte2);
 
             var fullPath = Path.GetDirectoryName(fileClassName);
             var fileNameWithOutExtension = Path.GetFileNameWithoutExtension(fileClassName);
@@ -45,12 +48,10 @@ namespace BuilderGenerator
          }
       }
 
-      private async Task<string> GetFileClassNameAsync()
+      private async Task<string> GetFileClassNameAsync(DTE2 dte2)
       {
          await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-         var dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
-
+         
          var selectedItems = dte2.SelectedItems;
          if (selectedItems == null)
          {
@@ -64,16 +65,39 @@ namespace BuilderGenerator
 
          //Get selected item
          var selectedItem = selectedItems.Item(1);
-
+         
          //and selectedItem.Project will not be null.
          var projectItem = selectedItem.ProjectItem;
-
+         var projectName = projectItem.Name;
          // Get the Ful filename
          var fileName = projectItem.FileNames[1];
 
+         var project = selectedItem.Project;
+
+         ProjectItem parentProjectItem = dte2.Solution.FindProjectItem(fileName);
+
+         VS.MessageBox.Show(parentProjectItem.Name, parentProjectItem.Kind);
+
+         //var project = projectItem.ContainingProject;
+
+         //if (project != null)
+         //{
+         //   var projectItems = project.ProjectItems;
+         //   int projectItemsCount = projectItems.Count;
+         //   ProjectItem item;
+         //   for (int i = 1; i <= projectItemsCount; i++ )
+         //   {
+         //      item = projectItems.Item(i);
+         //      VS.MessageBox.Show(item.Name, item.Kind);
+         //   }
+         //}
+
+
+         //project.ProjectItems.AddFromFile(fileBuildName).Properties.Item((object)"ItemType").Value = (object)"Compile";
+
+
 
          return fileName;
-
       }
    }
 }
