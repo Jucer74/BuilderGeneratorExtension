57fe72fd3a42684ab0de2da9816c201cdf040496
Julio Robles <Jucer74@Hotmail.com>
2022-07-18 23:47:39 -0500

    Update BuilderCommand.cs
    AddFileToProject Funtion aded

--- Code/src/Extension/Commands/BuilderCommand.cs
+++ Code/src/Extension/Commands/BuilderCommand.cs
@@ -18,29 +18,16 @@ namespace BuilderGenerator
 
          try
          {
-            //var dte2 = await ServiceProvider.GetGlobalServiceAsync(typeof(SDTE)) as DTE2;
             var dte2 = ServiceProvider.GlobalProvider.GetService(typeof(SDTE)) as DTE2;
-
-            var options = BuilderGeneratorOptions.Instance;
-
-            BuilderOptions builderOptions = new BuilderOptions()
-            {
-               PropertiesType = options.PropertierType,
-               EnableAutoFixture = options.EnableAutoFixture
-            };
 
             string fileClassName = await GetFileClassNameAsync(dte2);
 
-            var fullPath = Path.GetDirectoryName(fileClassName);
-            var fileNameWithOutExtension = Path.GetFileNameWithoutExtension(fileClassName);
+            BuilderOptions builderOptions = GetOptions();
 
-            string fullBuilderFileName = string.Format("{0}\\{1}Builder.cs", fullPath, fileNameWithOutExtension);
+            string fullBuilderFileName  = CreateBuilderFile(fileClassName, builderOptions);
 
-            string fileContent = File.ReadAllText(fileClassName);
+            AddFileToProjectAsync(dte2, fullBuilderFileName);
 
-            string builderClassContent = BuilderFactory.Generate(fileContent, builderOptions);
-
-            File.WriteAllText(fullBuilderFileName, builderClassContent);
          }
          catch (Exception ex)
          {
@@ -48,11 +35,53 @@ namespace BuilderGenerator
          }
       }
 
+      private async Task AddFileToProjectAsync(DTE2 dte2, string fullBuilderFileName)
+      {
+         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+         var parentDirectory = Path.GetDirectoryName(fullBuilderFileName);
+
+         ProjectItem parentProjectItem = dte2.Solution.FindProjectItem(parentDirectory);
+
+         parentProjectItem.ProjectItems.AddFromFile(fullBuilderFileName);
+      }
+
+      private static string CreateBuilderFile(string fileClassName, BuilderOptions builderOptions)
+      {
+         var fullPath = Path.GetDirectoryName(fileClassName);
+
+         var fileNameWithOutExtension = Path.GetFileNameWithoutExtension(fileClassName);
+
+         string fullBuilderFileName = string.Format("{0}\\{1}Builder.cs", fullPath, fileNameWithOutExtension);
+
+         string fileContent = File.ReadAllText(fileClassName);
+
+         string builderClassContent = BuilderFactory.Generate(fileContent, builderOptions);
+
+         File.WriteAllText(fullBuilderFileName, builderClassContent);
+
+         return fullBuilderFileName;
+      }
+
+      private static BuilderOptions GetOptions()
+      {
+         var options = BuilderGeneratorOptions.Instance;
+
+         BuilderOptions builderOptions = new BuilderOptions()
+         {
+            PropertiesType = options.PropertierType,
+            EnableAutoFixture = options.EnableAutoFixture
+         };
+
+         return builderOptions;
+      }
+
       private async Task<string> GetFileClassNameAsync(DTE2 dte2)
       {
          await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-         
+
          var selectedItems = dte2.SelectedItems;
+
          if (selectedItems == null)
          {
             throw new NoSelectedFileException("No Selected File");
@@ -65,37 +94,11 @@ namespace BuilderGenerator
 
          //Get selected item
          var selectedItem = selectedItems.Item(1);
-         
+
          //and selectedItem.Project will not be null.
          var projectItem = selectedItem.ProjectItem;
-         var projectName = projectItem.Name;
-         // Get the Ful filename
+         // Get the Full filename
          var fileName = projectItem.FileNames[1];
-
-         var project = selectedItem.Project;
-
-         ProjectItem parentProjectItem = dte2.Solution.FindProjectItem(fileName);
-
-         VS.MessageBox.Show(parentProjectItem.Name, parentProjectItem.Kind);
-
-         //var project = projectItem.ContainingProject;
-
-         //if (project != null)
-         //{
-         //   var projectItems = project.ProjectItems;
-         //   int projectItemsCount = projectItems.Count;
-         //   ProjectItem item;
-         //   for (int i = 1; i <= projectItemsCount; i++ )
-         //   {
-         //      item = projectItems.Item(i);
-         //      VS.MessageBox.Show(item.Name, item.Kind);
-         //   }
-         //}
-
-
-         //project.ProjectItems.AddFromFile(fileBuildName).Properties.Item((object)"ItemType").Value = (object)"Compile";
-
-
 
          return fileName;
       }
